8ebe5c39be7938ad3e1ac770fcc40f7dfb9610fb
AlpizarGregory <gregory.ac@estudiantec.cr>
2022-06-13 04:11:12 -0600

    Blocks setter finished

--- Server/jsonManager/jsonParser.c
+++ Server/jsonManager/jsonParser.c
@@ -1,16 +1,36 @@
+//······················································································································
+// Es necesario instalar librerias en consola para utilizar las funciones json de este archivo
+//
+// $ sudo apt install libjson-c-dev
+//
+// Tomado del tutorial: https://youtu.be/dQyXuFWylm4
+//
+// Comando para ejecutar programa desde consola:
+//
+// $ gcc fileName.c -ljson-c -o fileName
+//
+// $ ./fileName
+//
+// Nota: Es necesario estar en el directorio del archivo para ejecutarlo
+//······················································································································
+
 #include <stdio.h>
 #include <json-c/json.h>
 #include <stdlib.h>
 #include <time.h>
 
-size_t charSize = 10240;
+size_t charSize = 10240; // Tamaño del char que el server envía al cliente (texto de blocks.json)
 
-int getRandom(int upper) {
-
-    int randomNumber = (rand() % (upper + 1));
+// Descripción: Genera un número aleatorio dentro del rango solicitado
+// Parámetros: El valor mínimo y el valor máximo que puede tener el número aleatorio
+// Salida: Un entero aleatorio
+int getRandom(int lower, int upper) {
+    int randomNumber = (rand() % (upper - lower + 1)) + lower;
     return randomNumber;
 }
 
+// Descripción: Parser del archivo blocks.json
+// Salida: El char con toda la info del archivo json parseado
 char *parseJson() {
     FILE *fileInfo;
     char *jsonString = malloc(charSize);
@@ -22,6 +42,8 @@ char *parseJson() {
     return jsonString;
 }
 
+// Descripción: Reestablece los datos del archivo blocks.json
+// Coloca el puntaje de los 80 bloques en 0, y todos los estados (poderes) en "null"
 void resetJson() {
     struct json_object *root = json_object_from_file("data//blocks.json");
     struct json_object *blocks = json_object_object_get(root, "blocks");
@@ -38,19 +60,20 @@ void resetJson() {
     }
 }
 
-void newLevel() {
+// Descripción: Asigna valores de puntuación y poderes a cada uno de los bloques cuando se inicia una nueva partida
+void newGame() {
     struct json_object *root = json_object_from_file("data//blocks.json");
     struct json_object *blocks = json_object_object_get(root, "blocks");
     struct json_object *block, *points, *state;
     size_t i, j, counter;
     counter = 0;
 
-    srand(time(0));
+    srand(time(0)); // Con esto se obtienen enteros aleatorios distintos en cada ejecución del juego
 
-    for (i = 0; i < 4; i++) {
-        int levelPoints = getRandom(15);
-        for (j = 0; j < 20; j++) {
-            int powerProbability = getRandom(99);
+    for (i = 0; i < 4; i++) { // División por niveles
+        int levelPoints = getRandom(10, 25);
+        for (j = 0; j < 20; j++) { // Asignaciones para los bloques de un solo nivel (20 en total)
+            int powerProbability = getRandom(0, 99);
             block = json_object_array_get_idx(blocks, counter);
             points = json_object_object_get(block, "points");
             state = json_object_object_get(block, "state");
@@ -79,4 +102,3 @@ void newLevel() {
     json_object_to_file("data//blocks.json", root);
 
 }
-
