483e74ad585817e37e6b26025d91d47ef7f9429f
AlpizarGregory <gregory.ac@estudiantec.cr>
2022-06-13 03:44:58 -0600

    Block assigning ready

--- Server/jsonManager/jsonParser.c
+++ Server/jsonManager/jsonParser.c
@@ -1,3 +1,82 @@
-//
-// Created by alpizarg on 6/13/22.
-//
+#include <stdio.h>
+#include <json-c/json.h>
+#include <stdlib.h>
+#include <time.h>
+
+size_t charSize = 10240;
+
+int getRandom(int upper) {
+
+    int randomNumber = (rand() % (upper + 1));
+    return randomNumber;
+}
+
+char *parseJson() {
+    FILE *fileInfo;
+    char *jsonString = malloc(charSize);
+
+    fileInfo = fopen("data//blocks.json", "r");
+    fread(jsonString, charSize, 1, fileInfo);
+    fclose(fileInfo);
+
+    return jsonString;
+}
+
+void resetJson() {
+    struct json_object *root = json_object_from_file("data//blocks.json");
+    struct json_object *blocks = json_object_object_get(root, "blocks");
+    struct json_object *block, *points, *state;
+    size_t i;
+
+    for (i = 0; i < 80; i++) {
+        block = json_object_array_get_idx(blocks, i);
+        points = json_object_object_get(block, "points");
+        state = json_object_object_get(block, "state");
+        json_object_set_int(points, 0);
+        json_object_set_string(state, "null");
+        json_object_to_file("data//blocks.json", root);
+    }
+}
+
+void newLevel() {
+    struct json_object *root = json_object_from_file("data//blocks.json");
+    struct json_object *blocks = json_object_object_get(root, "blocks");
+    struct json_object *block, *points, *state;
+    size_t i, j, counter;
+    counter = 0;
+
+    srand(time(0));
+
+    for (i = 0; i < 4; i++) {
+        int levelPoints = getRandom(15);
+        for (j = 0; j < 20; j++) {
+            int powerProbability = getRandom(99);
+            block = json_object_array_get_idx(blocks, counter);
+            points = json_object_object_get(block, "points");
+            state = json_object_object_get(block, "state");
+            json_object_set_int(points, levelPoints);
+
+            if (powerProbability < 7) {
+                json_object_set_string(state, "moreSpeed");
+            } else if (powerProbability < 14) {
+                json_object_set_string(state, "lessSpeed");
+            } else if (powerProbability < 21) {
+                json_object_set_string(state, "doubleSize");
+            } else if (powerProbability < 28) {
+                json_object_set_string(state, "halfSize");
+            } else if (powerProbability < 35) {
+                json_object_set_string(state, "addBall");
+            } else if (powerProbability < 42) {
+                json_object_set_string(state, "addLive");
+            } else {
+                json_object_set_string(state, "null");
+            }
+
+            counter++;
+        }
+    }
+
+    json_object_to_file("data//blocks.json", root);
+
+}
+
--- Server/main.c
+++ Server/main.c
@@ -1,25 +1,28 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 #include <json-c/json.h>
+#include "jsonManager//jsonParser.c"
 
 void readJson() {
-    FILE  *fp;
-    char buffer[2048];
-
-    struct json_object *parsedJson;
-    struct json_object *blocks;
-    struct json_object *parsedBlocks;
-
-    struct json_object *friend;
-    struct json_object *block;
-
-    size_t n_friends;
-    size_t i;
-
-    fp = fopen("data//blocks.json", "r");
-    fread(buffer, 1024, 1, fp);
-    fclose(fp);
-
-    printf("%s", buffer);
+//    FILE  *fp;
+//    char buffer[2048];
+//
+//    struct json_object *parsedJson;
+//    struct json_object *blocks;
+//    struct json_object *parsedBlocks;
+//
+//    struct json_object *friend;
+//    struct json_object *block;
+//
+//    size_t n_friends;
+//    size_t i;
+//
+//    fp = fopen("data//blocks.json", "r");
+//    fread(buffer, 1024, 1, fp);
+//    fclose(fp);
+//
+//    printf("%s", buffer);
 
 //    parsedJson = json_tokener_parse(buffer);
 //    parsedBlocks = json_tokener_parse(json_object_get_string(parsedJson));
@@ -40,18 +43,37 @@ void readJson() {
 }
 
 void setJson() {
-    struct json_object *root = json_object_from_file("data//blocks.json");
-    printf("%s\n", json_object_get_string(root));
-    struct json_object *blocks = json_object_object_get(root, "blocks");
-    struct json_object *block = json_object_array_get_idx(blocks, 0);
-    struct json_object *points = json_object_object_get(block, "resistance");
+//    struct json_object *root = json_object_from_file("data//blocks.json");
+//    printf("%s\n", json_object_get_string(root));
+//    struct json_object *blocks = json_object_object_get(root, "blocks");
+//    struct json_object *block = json_object_array_get_idx(blocks, 0);
+//    struct json_object *points = json_object_object_get(block, "resistance");
 //    json_object_set_int(points, 35);
 //    json_object_to_file("data//blocks.json", root);
 }
 
 int main(int argc, char **argv) {
 //    readJson();
-    setJson();
+//    setJson();
+//    resetJson();
+
+
+//    for (int i = 0; i < 2; i++) {
+//        newLevel();
+//        struct json_object *parsedJson;
+//        char *jsonString = parseJson();
+//        parsedJson = json_tokener_parse(jsonString);
+//        printf("%s\n", json_object_get_string(parsedJson));
+//        resetJson();
+//    }
+
+
+//    srand(time(0));
+//    for (int i = 0; i < 80; i++) {
+//        int number = getRandom(100);
+//        printf("%d ", number);
+//    }
+//    newLevel();
 }
 
 
